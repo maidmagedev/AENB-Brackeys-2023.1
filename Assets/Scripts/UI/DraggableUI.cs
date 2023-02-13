dfa99d73fd88a94670442448323111cdc9469de1
Cheeseycube <58313569+Cheeseycube@users.noreply.github.com>
2023-02-13 16:53:09 -0500

    draggable ui

---
+++ Assets/Scripts/UI/DraggableUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableUI : MonoBehaviour, IDragHandler
+{
+    private Vector2 mousePosition = new Vector2();
+    private Vector2 startPosition = new Vector2();
+    private Vector2 differencePoint = new Vector2();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            UpdateMousePosition();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            UpdateStartPosition();
+            UpdateDifferencePoint();
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        /*Minus the difference point so you can pick the 
+        element from the edges, without any jerk*/
+
+        transform.position = mousePosition - differencePoint;
+    }
+
+    private void UpdateMousePosition()
+    {
+        mousePosition.x = Input.mousePosition.x;
+        mousePosition.y = Input.mousePosition.y;
+    }
+
+    private void UpdateStartPosition()
+    {
+        startPosition.x = transform.position.x;
+        startPosition.y = transform.position.y;
+    }
+
+    private void UpdateDifferencePoint()
+    {
+        differencePoint = mousePosition - startPosition;
+    }
+}
+
