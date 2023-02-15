9608ea19771da6797bbb77c3579c52111fc23ea8
crwohlers <101900065+crwohlers@users.noreply.github.com>
2023-02-15 15:50:28 -0500

    BELTS

--- Assets/Scripts/Factory/Machines/Belt.cs
+++ Assets/Scripts/Factory/Machines/Belt.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,103 @@ public class Belt : Machine, IODevice
 {
     private ItemCollection IOBuf = new(6);
 
-    private Orientation orientation;
+    public Orientation orientation;
+
+    float delay = .5f;
 
     public Belt(){
         type = MachineType.BELT;
         footPrint = new(1,1);
-    }
+        child_start = BeltStart;
 
-    private void Update()
+    }
+    
+    Vector2Int intendPushTo;
+
+    public Sprite def;
+
+    void BeltStart()
     {
-        //see grabber, but belt only
+        intendPushTo = new Vector2Int((int)position.x, (int)position.y) + OrientationHelper.pushToBind[orientation];
     }
 
+    public override void Update()
+    {
+        if(IOBuf[0] != null){
+            GetComponent<SpriteRenderer>().sprite = Item.item_definitions[IOBuf[0].of].sprite;
+        }
+        else{
+            GetComponent<SpriteRenderer>().sprite = def;
+        }
+
+        if (!working){
+            TileData putTo;
+
+            TileManager.tileData.TryGetValue(intendPushTo, out putTo);
+
+            Machine put = putTo != null && putTo.occupiedBy.type == MachineType.BELT ? putTo.occupiedBy : null;
+
+            if(put != null){
+                
+                ItemStack items = null;
+
+
+                if (IOBuf[0] != null){
+                    working = true;
+                    StartCoroutine(doCooldown(()=>{
+                        items = put.getInputBuffer().Add(IOBuf[0]).more;
+                        
+                        if (items != null){
+                            int transferred = IOBuf[0].quantity - items.quantity;
+                            IOBuf.Remove(new ItemStack(items.of, transferred));
+                        }
+                        else if (IOBuf[0] != null){
+                    
+                            IOBuf.Remove(IOBuf[0]); 
+                            advanceBelt();
+                        }
+                    }));
+                    
+                }
+
+                
+
+                
+            }
+        }
+    }
+
+
+    private float workingTime = 0;
+
+    public IEnumerator doCooldown(Action onFinish){
+        while (workingTime < delay)
+        {
+            workingTime += Time.deltaTime;
+            //print("frame");
+            yield return null;
+        }
+
+        onFinish.Invoke();
+        workingTime = 0;
+        working = false;
+        //print("workFin");
+    }
+
+
+    public void advanceBelt(){
+        if(IOBuf[0] != null){
+            throw new Exception("push failed?");
+        }
+        else{
+            for (int i = 0; i < IOBuf.Size - 2; i++){
+                IOBuf[i] = IOBuf[i+1];
+            }
+
+            IOBuf[IOBuf.Size -1] = null;
+        }
+    }
+    
     public override ItemCollection getInputBuffer()
     {
         return IOBuf;
