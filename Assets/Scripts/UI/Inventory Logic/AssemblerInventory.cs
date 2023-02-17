64a08a054e0a8aaa2543e2eef2abebf2fd1b9fae
maidmage <46210759+glockmage@users.noreply.github.com>
2023-02-16 23:25:22 -0500

    Assembler Menu functionality
    Assembler menu now works identical to the furnace inventory menu, activate via an Enum in the PlayerInventoryUI. To be managed via separate scripts.

--- Assets/Scripts/UI/Inventory Logic/AssemblerInventory.cs
+++ Assets/Scripts/UI/Inventory Logic/AssemblerInventory.cs
@@ -8,11 +8,12 @@ public class AssemblerInventory : BaseInventory
         inventory_grid = new Dictionary<int, InventoryElement>()
         {
             // this arrangement is based off the furnace inventory and should be changed
-            {0, new ItemSlot(new Vector3(23.9f, -25.75f, 0))}, // input buffer?
-            {1, new ItemSlot(new Vector3(-4.2f, 5.8f, 0))},    // input buffer?
-            {2, new ItemSlot(new Vector3(23.9f, 5.8f, 0))},    // output buffer
+            {0, new ItemSlot(new Vector3(-6.2f, -25.75f, 0))}, // Input  Buffer - 1
+            {1, new ItemSlot(new Vector3(10.5f, -25.75f, 0))}, // Input  Buffer - 2
+            {2, new ItemSlot(new Vector3(26.9f, -25.75f, 0))},    // Input  Buffer - 3
+            {3, new ItemSlot(new Vector3(10.5f, 5.8f, 0))},    // Output Buffer - Output Item
         };
         
-        inventory = new ItemCollection(3);
+        inventory = new ItemCollection(4);
     }
 }
--- Assets/Scripts/UI/Inventory Logic/FurnaceInventory.cs
+++ Assets/Scripts/UI/Inventory Logic/FurnaceInventory.cs
@@ -7,9 +7,9 @@ public class FurnaceInventory : BaseInventory
     public FurnaceInventory() {
         inventory_grid = new Dictionary<int, InventoryElement>()
         {
-            {0, new ItemSlot(new Vector3(23.9f, -25.75f, 0))}, // Input  Buffer - Fuel
-            {1, new ItemSlot(new Vector3(-4.2f, 5.8f, 0))},    // Input  Buffer - Input Item
-            {2, new ItemSlot(new Vector3(23.9f, 5.8f, 0))},    // Output Buffer - Output Item
+            {0, new ItemSlot(new Vector3(26.9f, -25.75f, 0))}, // Input  Buffer - Fuel
+            {1, new ItemSlot(new Vector3(-6.2f, 5.8f, 0))},    // Input  Buffer - Input Item
+            {2, new ItemSlot(new Vector3(26.9f, 5.8f, 0))},    // Output Buffer - Output Item
         };
         
         inventory = new ItemCollection(3);
