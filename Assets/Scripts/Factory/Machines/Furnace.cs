be0f75c308b2feccbb24a136191ace59b159a37a
Joseph Rousoss <jroussos01@gmail.com>
2023-02-17 20:33:10 -0500

    furnace changes

--- Assets/Scripts/Factory/Machines/Furnace.cs
+++ Assets/Scripts/Factory/Machines/Furnace.cs
@@ -15,20 +15,14 @@ public class Furnace : Machine
         type = MachineType.FURNACE;
         footPrint = new(2, 2);
         child_start = Furnace_Start;
-        child_update = Furnace_Update;
     }
 
     private void Furnace_Start()
     {
-        inpBuf.Add(new ItemStack(ItemType.COAL, 5));
         // automatically searches for the items needed to complete the given recipe and consumes them and goes into the output buffer
         doing = new Recipe(Globals.allRecipes["ironOreToBar"]);
     }
-
-    private void Furnace_Update()
-    {
-        
-    }
+    
 
     public void Set_Recipe(Recipe recipe)
     {
