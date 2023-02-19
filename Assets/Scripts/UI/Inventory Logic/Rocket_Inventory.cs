d5aaa758657dee5042a78aa5c8701120fe19cca6
Joseph Rousoss <jroussos01@gmail.com>
2023-02-19 18:51:10 -0500

    fixed rocket inventory

--- Assets/Scripts/UI/Inventory Logic/Rocket_Inventory.cs
+++ Assets/Scripts/UI/Inventory Logic/Rocket_Inventory.cs
@@ -9,10 +9,10 @@ public class Rocket_Inventory : BaseInventory
         inventory_grid = new Dictionary<(int, int), InventoryElement>()
         {
             // this arrangement is based off the furnace inventory and should be changed
-            {(0,0), new ItemSlot(new Vector3(-6.26f, -25.78f, 0))}, // Input  Buffer - 1
-            {(0,1), new ItemSlot(new Vector3(10.35f, -25.78f, 0))}, // Input  Buffer - 2
-            {(0,2), new ItemSlot(new Vector3(26.96f, -25.75f, 0))}, // Input  Buffer - 3
-            {(0, 3), new ItemSlot(new Vector3(44.25f, -25.75f, 0))} // Input Buffer - 4
+            {(0,0), new ItemSlot(new Vector3(-25f, -25.78f, 0))}, // Input  Buffer - 1
+            {(0,1), new ItemSlot(new Vector3(-8.37f, -25.81f, 0))}, // Input  Buffer - 2
+            {(0,2), new ItemSlot(new Vector3(8.3f, -25.75f, 0))}, // Input  Buffer - 3
+            {(0, 3), new ItemSlot(new Vector3(24.96f, -25.81f, 0))} // Input Buffer - 4
         };
         
         inventory.Add(new ItemCollection(12));
