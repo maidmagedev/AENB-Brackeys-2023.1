7a817204ea10f398442a88d7c5149474c10f30b1
crwohlers <101900065+crwohlers@users.noreply.github.com>
2023-03-12 21:31:58 -0400

    second

--- Assets/Scripts/Player/PlayerAniEvents.cs
+++ Assets/Scripts/Player/PlayerAniEvents.cs
@@ -7,14 +7,6 @@ public class PlayerAniEvents : MonoBehaviour
 {
     [SerializeField] TopDownMovementComponent topDownMovementComponent;
 
-    void FreezePlayer() {
-        topDownMovementComponent.DisableMovement();
-    }
-
-    void UnFreezePlayer() {
-        topDownMovementComponent.EnableMovement();
-    }
-
     void SetPlayerMovement(float ms) {
         topDownMovementComponent.movementSpeed = ms;
     }
---
+++ Assets/Scripts/PlayerVar.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Player")]
+public class PlayerVar : ScriptableObject
+{
+    public GameObject player;
+}
