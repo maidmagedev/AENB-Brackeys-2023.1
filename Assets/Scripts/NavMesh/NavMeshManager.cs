ad300ef51b15d95347d3c9775e23f924d95097e9
Joseph Rousoss <jroussos01@gmail.com>
2023-03-13 17:16:27 -0400

    navmesh maybe?

--- Assets/Scripts/NavMesh/NavMeshManager.cs
+++ Assets/Scripts/NavMesh/NavMeshManager.cs
@@ -13,7 +13,7 @@ public class NavMeshManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Bake();
+        //Bake();
     }
 
     // Update is called once per frame
